d5b0c01037389b699025522866e8652751730b01
Tommypyt <tgiammusso87@gmail.com>
2019-12-12 12:47:42 +0100

    #2 lookfor config via cli or in cwd

--- cli-airplay.py
+++ cli-airplay.py
@@ -2,34 +2,52 @@ import airplay
 import argparse
 import json
 import sys
+import os
+import yaml
 
 argv = sys.argv
-
 parser = argparse.ArgumentParser(description="Command line interface for AirTable")
-parser.add_argument("table", type=str, help="Table name")
-parser.add_argument("base", type=str, help="Base key")
-parser.add_argument("apikey", type=str, help="API key")
-parser.add_argument("action", type=str, help="Chose an action", choices=["ins", "mod", "del", "get"])
-parser.add_argument("-payload", type=str, help="Payload dictionary")
-parser.add_argument("-target_id", type=str, help="Target ID")
-# parser.set_defaults(payload={'Name':'test', 'Code':100})
+parser.add_argument("table_name", help="Table name")
+parser.add_argument("action", help="Chose an action", choices=["ins", "mod", "del", "get"])
+parser.add_argument("-payload", help="Payload dictionary")
+parser.add_argument("-target_id", help="Target ID")
+parser.add_argument("-c", help="Config file path")
 args = parser.parse_args()
-tbl = airplay.Table(args.base, args.table, args.apikey)
+
+
+def chk_config_():
+    if os.path.isfile('config.yaml'):
+        cf = 'config.yaml'
+    elif '-c' in sys.argv:
+        cf = args.c
+    else:
+        cf = None
+        tbl = None
+        print("Missing or invalid configuration file")
+    if cf is not None:
+        with open(cf, 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+            tbl = airplay.Table(config['base_key'], args.table_name, config['api_key'])
+    return tbl
 
 
 def actions():
-    if args.action == "ins":
-        dd = json.loads(args.payload)
-        print(tbl.insert(**dd))
-    if args.action == "get":
-        print(tbl.get())
-        return tbl.get()
-    if args.action == "mod":
+    if chk_config_() is not None:
+        tbl = chk_config_()
+        if args.action == "ins":
+            dd = json.loads(args.payload)
+            print(tbl.insert(**dd))
+        if args.action == "get":
+            print(tbl.get())
+            return tbl.get()
+        if args.action == "mod":
+            pass
+            dd = json.loads(args.payload)
+            print(tbl.modify(args.target_id, **dd))
+        if args.action == "del":
+            print(tbl.delete(args.target_id))
+    else:
         pass
-        dd = json.loads(args.payload)
-        print(tbl.modify(args.target_id, **dd))
-    if args.action == "del":
-        print(tbl.delete(args.target_id))
 
 
 actions()
